c3a0c7a428645e8cf08e9003cb0ef341f5984c3b
tresespadas <alvaro8038c@gmail.com>
2024-12-03 19:40:04 +0100

    first commit

---
+++ bash_scripting/nivel1/ej1_nivel1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# AUTOR: Álvaro | MÓDULO: Nivel 1 | FECHA: 3/12/24
+# Programa que reciba tres palabras como parámetros y las visualice al revés
+
+if [ "$#" -ne 3 ]
+  then
+    echo "No es igual a 3"
+  else
+    echo "Es igual a 3"
+fi
