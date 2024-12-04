231cf551c14ec47f699759b5dc3e8d99b035d1b3
Álvaro <alvaro8038c@gmail.com>
2024-12-04 12:47:30 +0100

    ejercicio de clase

--- bash_scripting/ejerciciosClase/usuarios.sh
+++ bash_scripting/ejerciciosClase/usuarios.sh
@@ -3,40 +3,45 @@
 
 if [[ $(whoami) == "root" ]]
 	then
-		echo "Hola $(whoami)"  # Testeando el else - BORRAR
 		while true; do
 			echo -e "\n 1.- Crear usuario\n 2.- Borrar usuario\n 3.- Crear grupo\n 4.- Añadir usuario a un grupo\n 5.- Salir\n"
 			read -p "Introduce la opción elegida: " option
 			break
 		done
-		echo "Hola, $option" # Testeando el break anterior - BORRAR
 		case $option in
 			1)
-				echo "Esta es la opción $option" # Testeando que entra aquí - BORRAR
 				read -p "Nombre del nuevo usuario: " new_user
-				echo "$new_user" # Testeando la variable user - BORRAR
 				cat /etc/passwd | grep -iw "^$new_user" &> /dev/null
 				if [[ $? -eq 0 ]]
 				then
 					echo "El usuario existe"
 				else
-					echo "El usuario no existe"
-          #useradd
-          read -p -s "Elige una contraseña para el nuevo usuario: " new_user_pass #ARREGLAR "-s"
-          #sudo useradd -m -p ($new_user_pass $new_user
+          read -sp "Elige una contraseña para el nuevo usuario: " new_user_pass
+          echo
+          sudo useradd -m -p $new_user_pass $new_user
 				fi
 				;;
 			2)
 				echo "Esta es la opción $option"
+        read -p "Nombre del usuario que se desea borrar: " $user_del
+        echo $user_del
+				cat /etc/passwd | grep -iw "^$user_del" &> /dev/null
+				if [[ $? -eq 0 ]]
+				then
+					echo "El usuario existe"
+          sudo userdel $user_del # ARREGLAR - No funciona
+				else
+					echo "El usuario no existe"
+				fi
 				;;
 			3)
-				echo "Esta es la opción $option"
+				echo "Esta es la opción $option" # Opcion de crear grupo
 				;;
 			4)
-				echo "Esta es la opción $option"
+				echo "Esta es la opción $option" # Opcion de añadir usuario a grupo
 				;;
 			5)
-				echo "Esta es la opción $option"
+        exit 0
 				;;
 		esac
 	else
