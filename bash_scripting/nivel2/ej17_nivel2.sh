6163e3cfc2bd4c27cede60acf0958ae7a28b0c0a
tresespadas <alvaro8038c@gmail.com>
2024-12-09 00:43:09 +0100

    18/23

---
+++ bash_scripting/nivel2/ej17_nivel2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ] || [ "$#" -eq 1 ]; then
+  echo "Especifica dos archivos"
+  exit 1
+fi
+
+file1=$(find / -type f -name "$1" 2>/dev/null)
+file2=$(find / -type f -name "$2" 2>/dev/null)
+if [ -z "$file1" ] || [ -z "$file2" ]; then
+  echo "Especifica archivos que existan"
+  exit 2
+fi
+
+size1=$(stat "$file1" | grep "Size" | awk '{print $2}')
+size1_kb=$((size1/1024))
+
+size2=$(stat "$file2" | grep "Size" | awk '{print $2}')
+size2_kb=$((size2/1024))
+
+echo "$size1_kb" # El problema de la division es que no muestra decimales, por eso a veces sale 0KB
+echo "$size2_kb"
