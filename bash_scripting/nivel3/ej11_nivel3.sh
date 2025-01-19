bd5b3f7cbe3a0f394cc7378891afb9d5f9924a1a
Álvaro <alvaro8038c@gmail.com>
2025-01-19 23:33:41 +0100

    ej11

--- bash_scripting/nivel3/ej11_nivel3.sh
+++ bash_scripting/nivel3/ej11_nivel3.sh
@@ -5,10 +5,18 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-# cat /etc/passwd | awk -F ":" '$3 ~ /....+/ {print $1}'
-users[@]=$(cat /etc/passwd | awk -F ":" '$3 ~ /....+/ {print $1}')
+users=($(awk -F ":" '$3 ~ /....+/ {print $1}' /etc/passwd))
 
-echo $users
-# for arg in users[@]; do
-#   echo $users[arg]
-# done
+for user in "${users[@]}"; do
+  if [ "$1" == $user ]; then
+    echo "La carpeta personal del usuario $user está en $(awk -F ':' -v user=$user '$1 == user {print $6}' /etc/passwd)"
+    echo "El UID del usuario $user es $(awk -F ':' -v user=$user '$1 == user {print $3}' /etc/passwd)"
+    echo "El shell que el usuario $user ejecuta es $(awk -F ":" -v user=$user '$1 == user {print $7}' /etc/passwd)"
+    user_group=$(awk -F ":" -v user=$user '$1 == user {print $4}' /etc/passwd)
+    echo "El grupo principal al que pertenece el usuario $user es $(awk -F ":" -v group=$user_group '$3 == group {print $1}' /etc/group)"
+    exit 0
+  fi
+done
+
+echo "El usuario no está registrado en el sistema"
+exit 1
