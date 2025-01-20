ba82fc18910e199478c1017247fed30e44937e8c
Álvaro <alvaro8038c@gmail.com>
2025-01-20 12:26:41 +0100

    13/16

--- bash_scripting/nivel3/ej13_nivel3.sh
+++ bash_scripting/nivel3/ej13_nivel3.sh
@@ -6,13 +6,23 @@ if [ "$#" -ne 1 ]; then
 fi
 
 palabra=($(echo "$1" | sed 's/./& /g'))
+vocal_count=0
+consonante_count=0
 
 
 for i in "${palabra[@]}"; do
   if [[ "$i" =~ [aeiouAEIOU] ]]; then
-    echo "$i es una vocal"
+    ((vocal_count++))
+    # echo "$i es una vocal"
   fi
   if [[ ! "$i" =~ [aeiouAEIOU] ]]; then
-    echo "$i es una consonante"
+    if [[ "$i" =~ [0-9] ]]; then
+      continue
+    else
+      ((consonante_count++))
+      # echo "$i es una consonante"
+    fi
   fi
 done
+
+echo "Hay $vocal_count vocales y $consonante_count consonantes"
