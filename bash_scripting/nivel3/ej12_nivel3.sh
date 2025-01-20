742ed58d197ab1fc46a855160db6ec69c98d6361
Álvaro <alvaro8038c@gmail.com>
2025-01-20 10:33:26 +0100

    12/16

--- bash_scripting/nivel3/ej12_nivel3.sh
+++ bash_scripting/nivel3/ej12_nivel3.sh
@@ -1,9 +1,56 @@
 #!/bin/env bash
 
-echo "*** MENÚ ***"
-echo "1. Usuarios conectados al sistema"
-echo "2. Procesos que ejecuta un usuario"
-echo "3. Uso de memoria que está haciendo el sistema"
-echo "4. Uso de espacio de disco"
-echo "5. Salir del menú"
-read -p "Introduce una opción: " opt
+user_connected() {
+  who -q | grep -v =
+}
+
+user_process() {
+  ps au
+}
+
+user_mem() {
+  free -h | grep "Memoria:" | awk '{print $3}'
+}
+
+user_disk() { # Duda
+  du -sh / 2>/dev/null | awk '{print $1}'
+}
+
+exit_menu() {
+  echo "Saliendo..."
+  sleep 0.5
+  exit 0
+}
+
+while true; do
+  echo "*** MENÚ ***"
+  echo "1. Usuarios conectados al sistema"
+  echo "2. Procesos que ejecuta un usuario"
+  echo "3. Uso de memoria que está haciendo el sistema"
+  echo "4. Uso de espacio de disco"
+  echo "5. Salir del menú"
+  read -p "Introduce una opción: " opt
+
+  case "$opt" in
+    1)
+      user_connected;
+      ;;
+    2)
+      user_process;
+      ;;
+    3)
+      user_mem;
+      ;;
+    4)
+      user_disk;
+      ;;
+    5)
+      exit_menu;
+      ;;
+    *) 
+      echo "Opción inválidad"
+      ;;
+  esac
+done
+
+
